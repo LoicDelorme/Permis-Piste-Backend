5c0edf8c67b8c421982ad8c5f2e1612bd3fa13a0
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-05-27 14:56:27 +0200

    Add "User" entity representation

---
+++ src/main/java/fr/polytech/permispiste/entities/User.java
@@ -0,0 +1,111 @@
+package fr.polytech.permispiste.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ * This class represents a user entity.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "email") })
+public class User implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@Column(name = "id")
+	private int id;
+
+	@NotNull
+	@Column(name = "lastname")
+	private String lastname;
+
+	@NotNull
+	@Column(name = "firstname")
+	private String firstname;
+
+	@NotNull
+	@Column(name = "email")
+	private String email;
+
+	@NotNull
+	@Column(name = "password")
+	private String password;
+
+	@NotNull
+	@Column(name = "isEnabled")
+	private boolean isEnabled;
+
+	@NotNull
+	@Column(name = "is_administrator")
+	private boolean isAdministrator;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLastname() {
+		return this.lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return this.firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public boolean isEnabled() {
+		return this.isEnabled;
+	}
+
+	public void setEnabled(boolean isEnabled) {
+		this.isEnabled = isEnabled;
+	}
+
+	public boolean isAdministrator() {
+		return this.isAdministrator;
+	}
+
+	public void setAdministrator(boolean isAdministrator) {
+		this.isAdministrator = isAdministrator;
+	}
+}

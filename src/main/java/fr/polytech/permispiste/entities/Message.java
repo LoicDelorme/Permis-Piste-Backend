159335dddc2eafacc011bc74777de703c64fe69f
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-05-27 15:45:29 +0200

    Add "Message" entity representation

---
+++ src/main/java/fr/polytech/permispiste/entities/Message.java
@@ -0,0 +1,102 @@
+package fr.polytech.permispiste.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ * This class represents a message entity.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "messages")
+public class Message implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@Column(name = "id")
+	private int id;
+
+	@NotNull
+	@Column(name = "subject")
+	private String subject;
+
+	@NotNull
+	@Column(name = "body")
+	private String body;
+
+	@NotNull
+	@Column(name = "date")
+	private LocalDateTime date;
+
+	@NotNull
+	@Column(name = "is_read")
+	private boolean isRead;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@NotNull
+	@Column(name = "user")
+	private User user;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getSubject() {
+		return this.subject;
+	}
+
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+
+	public String getBody() {
+		return this.body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	public LocalDateTime getDate() {
+		return this.date;
+	}
+
+	public void setDate(LocalDateTime date) {
+		this.date = date;
+	}
+
+	public boolean isRead() {
+		return this.isRead;
+	}
+
+	public void setRead(boolean isRead) {
+		this.isRead = isRead;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}

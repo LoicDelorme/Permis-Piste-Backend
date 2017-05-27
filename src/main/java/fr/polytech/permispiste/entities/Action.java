83cb31b684567ef09c389fa6781f345e3055fcf6
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-05-27 14:57:03 +0200

    Add "Action" entity representation

---
+++ src/main/java/fr/polytech/permispiste/entities/Action.java
@@ -0,0 +1,51 @@
+package fr.polytech.permispiste.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ * This class represents an action entity.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "actions", uniqueConstraints = { @UniqueConstraint(columnNames = "label") })
+public class Action implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@Column(name = "id")
+	private int id;
+
+	@NotNull
+	@Column(name = "label")
+	private String label;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+}

a7b168bdedfa0dbe11aa7f556dff87a975e1ec5d
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-05-27 15:01:20 +0200

    Add "Training" entity representation

---
+++ src/main/java/fr/polytech/permispiste/entities/Training.java
@@ -0,0 +1,75 @@
+package fr.polytech.permispiste.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+
+/**
+ * This class represents a training entity.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "trainings", uniqueConstraints = { @UniqueConstraint(columnNames = "label") })
+public class Training implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@NotNull
+	@Column(name = "id")
+	private int id;
+
+	@NotNull
+	@Column(name = "label")
+	private String label;
+
+	@NotNull
+	@Column(name = "description")
+	private String description;
+
+	@NotNull
+	@Column(name = "image_path")
+	private String imagePath;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getLabel() {
+		return this.label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImagePath() {
+		return this.imagePath;
+	}
+
+	public void setImagePath(String imagePath) {
+		this.imagePath = imagePath;
+	}
+}

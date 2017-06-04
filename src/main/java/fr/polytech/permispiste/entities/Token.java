515fec8c1a9c119b3894104944c1382f6931d1ca
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-06-04 11:06:55 +0200

    Add "Token" entity representation

---
+++ src/main/java/fr/polytech/permispiste/entities/Token.java
@@ -0,0 +1,79 @@
+package fr.polytech.permispiste.entities;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+
+/**
+ * This class represents a token entity.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
+@Entity
+@Table(name = "tokens")
+public class Token implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+	@Column(name = "id")
+	private int id;
+
+	@NotNull
+	@Column(name = "begin")
+	private LocalDateTime begin;
+
+	@NotNull
+	@Column(name = "end")
+	private LocalDateTime end;
+
+	@ManyToOne(cascade = CascadeType.ALL)
+	@NotNull
+	@Column(name = "user")
+	private User user;
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getBegin() {
+		return this.begin;
+	}
+
+	public void setBegin(LocalDateTime begin) {
+		this.begin = begin;
+	}
+
+	public LocalDateTime getEnd() {
+		return this.end;
+	}
+
+	public void setEnd(LocalDateTime end) {
+		this.end = end;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+}

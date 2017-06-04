22b33b3522925fc547109a4c7c440c67830d8a86
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-06-04 11:29:42 +0200

    Add an AbstractDaoServices to specify a generic behavior

---
+++ src/main/java/fr/polytech/permispiste/services/AbstractDaoServices.java
@@ -0,0 +1,76 @@
+package fr.polytech.permispiste.services;
+
+import static fr.polytech.permispiste.persistance.HibernateSessionManager.getSession;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Session;
+
+/**
+ * This class represents an abstract DAO services.
+ *
+ * @author DELORME Loïc
+ * @param <T>
+ *            The type of the object to handle.
+ * @since 1.0.0
+ */
+public class AbstractDaoServices<T> implements DaoServices<T> {
+
+	private final Class<T> entityClass;
+
+	public AbstractDaoServices(Class<T> entityClass) {
+		this.entityClass = entityClass;
+	}
+
+	@Override
+	public T get(Object id) {
+		final Session session = getSession();
+
+		session.beginTransaction();
+		final T entity = session.find(this.entityClass, id);
+		session.getTransaction().commit();
+
+		return entity;
+	}
+
+	@Override
+	public List<T> getAll() {
+		final Session session = getSession();
+
+		session.beginTransaction();
+		final CriteriaQuery<T> criteriaQuery = session.getCriteriaBuilder().createQuery(this.entityClass);
+		final List<T> entities = session.createQuery(criteriaQuery.select(criteriaQuery.from(this.entityClass))).getResultList();
+		session.getTransaction().commit();
+
+		return entities;
+	}
+
+	@Override
+	public void insert(T object) {
+		final Session session = getSession();
+
+		session.beginTransaction();
+		session.saveOrUpdate(object);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public void update(T object) {
+		final Session session = getSession();
+
+		session.beginTransaction();
+		session.update(object);
+		session.getTransaction().commit();
+	}
+
+	@Override
+	public void delete(T object) {
+		final Session session = getSession();
+
+		session.beginTransaction();
+		session.delete(object);
+		session.getTransaction().commit();
+	}
+}

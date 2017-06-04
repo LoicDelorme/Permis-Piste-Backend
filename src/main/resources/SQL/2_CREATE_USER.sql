608470e51946c92022b9b4c20b56a5ba8500d422
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-06-04 10:31:41 +0200

    Replace comment character by the good one

--- src/main/resources/SQL/2_CREATE_USER.sql
+++ src/main/resources/SQL/2_CREATE_USER.sql
@@ -1,3 +1,6 @@
-# DROP USER 'SpringAppUser'@'localhost';
+/*
+DROP USER 'SpringAppUser'@'localhost';
+*/
+
 CREATE USER 'SpringAppUser'@'localhost' IDENTIFIED BY 'SpringAppUser';
 GRANT SELECT, INSERT, UPDATE, DELETE ON *.* TO 'SpringAppUser'@'localhost' REQUIRE NONE WITH MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;

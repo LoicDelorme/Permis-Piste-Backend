ceefe80b13dbf7239f2f033b1480bee37fd082af
DELORME Loïc <loic.delorme.pro@gmail.com>
2017-06-17 13:44:04 +0200

    Add documentation + change java identifiers order

--- src/main/java/fr/polytech/permispiste/converters/JsonSerializer.java
+++ src/main/java/fr/polytech/permispiste/converters/JsonSerializer.java
@@ -3,9 +3,15 @@ package fr.polytech.permispiste.converters;
 import javax.json.bind.Jsonb;
 import javax.json.bind.JsonbBuilder;
 
+/**
+ * This class represents a JSON serializer.
+ *
+ * @author DELORME Loïc
+ * @since 1.0.0
+ */
 public class JsonSerializer implements Serializer<String> {
 
-	public final static Jsonb JSON_BUILDER = JsonbBuilder.create();
+	public static final Jsonb JSON_BUILDER = JsonbBuilder.create();
 
 	@Override
 	public String toT(Object obj) {

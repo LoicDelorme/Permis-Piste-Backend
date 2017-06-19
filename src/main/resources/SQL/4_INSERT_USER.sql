5fce0b260e67c80aaafe14412161b4a31f6bf85f
agory <gory.alexandre02@gmail.com>
2017-06-19 11:16:14 +0200

    add war

---
+++ src/main/resources/SQL/4_INSERT_USER.sql
@@ -0,0 +1,7 @@
+USE TP_PermisPiste;
+
+INSERT INTO users VALUES (1,'admin','admin','admin@polytech.fr','admin',1,1);
+INSERT INTO users VALUES (1,'alexandre','gory','alex.gory02@gmail.com','test',1,1);
+INSERT INTO users VALUES (1,'Loic','Delorme','loic.delorme@etu.univ-lyon1.fr','test',1,1);
+INSERT INTO users VALUES (1,'Nicolas','Diab','nicolas.diab@etu.univ-lyon1.fr','test',1,1);
+INSERT INTO users VALUES (1,'Christian','Vial','christian.vial@etu.univ-lyon1.fr','test',1,1);
